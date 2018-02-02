90164636ee8f3ced611ff338a3a513fb8082e555
Dmytro Tykhyi <dima.quites@gmail.com>
2018-02-02 13:47:43 -0800

    Optimisation of the calculation function

--- 10DaysJavascript/BinaryCalculator/js/binaryCalculator.js
+++ 10DaysJavascript/BinaryCalculator/js/binaryCalculator.js
@@ -1,10 +1,12 @@
+// Global Variables
 var selectedOper = "";
+let validOperRE = new RegExp(/^\d{0,}[^-+\/*]$/);
+let validEqualRE = new RegExp(/^\d{1,}[-+\/*]{1}\d{1,}$/);
 
+// Button Click function to orchestrate operations
 function clickButton(text) {
     let field = document.getElementById("res");
     let fieldText = field.innerHTML;
-    let validOperRE = new RegExp(/^\d{0,}[^-+\/*]$/);
-    let validEqualRE = new RegExp(/^\d{1,}[-+\/*]{1}\d{1,}$/);
     switch (text) {
             case '0':
             case '1':
@@ -24,38 +26,43 @@ function clickButton(text) {
             break;
         case '=':
             if (validEqualRE.test(fieldText)) {
-                let operands = fieldText.split(selectedOper.charAt(0));
-                let res = "";
-                switch (selectedOper.charAt(0)) {
-                    case '+':
-                        res = `${parseInt(operands[0], 2) + parseInt(operands[1], 2)}`;
-                        field.innerHTML = `${parseInt(res, 10).toString(2)}`;
-                        break;
-                    case '-':
-                        res = `${parseInt(operands[0], 2) - parseInt(operands[1], 2)}`;
-                        field.innerHTML = `${parseInt(res, 10).toString(2)}`;
-                        break;
-                    case '*':
-                        res = `${parseInt(operands[0], 2) * parseInt(operands[1], 2)}`;
-                        field.innerHTML = `${parseInt(res, 10).toString(2)}`;
-                        break;
-                    case '/':
-                        if (parseInt(operands[1], 2) > 0) {
-                            res = `${parseInt(operands[0], 2) / parseInt(operands[1], 2)}`;               
-                        } else {
-                            res = 0;
-                        }
-                        
-                        field.innerHTML = `${parseInt(res, 10).toString(2)}`;             
-
-                        break;
-
-                    default:
-                        break;
-                }
+               calculate(fieldText);
             }
         break;
             default:
                 break;
         }
+}
+
+// Calculation of the binary operation result
+function calculate(expression) {
+    let field = document.getElementById("res");
+    let operands = expression.split(selectedOper.charAt(0));
+    let res = "";
+    switch (selectedOper.charAt(0)) {
+        case '+':
+            res = `${parseInt(operands[0], 2) + parseInt(operands[1], 2)}`;
+            field.innerHTML = `${parseInt(res, 10).toString(2)}`;
+            break;
+        case '-':
+            res = `${parseInt(operands[0], 2) - parseInt(operands[1], 2)}`;
+            field.innerHTML = `${parseInt(res, 10).toString(2)}`;
+            break;
+        case '*':
+            res = `${parseInt(operands[0], 2) * parseInt(operands[1], 2)}`;
+            field.innerHTML = `${parseInt(res, 10).toString(2)}`;
+            break;
+        case '/':
+            if (parseInt(operands[1], 2) > 0) {
+                res = `${parseInt(operands[0], 2) / parseInt(operands[1], 2)}`;               
+            } else {
+                res = 0;
+            }
+
+            field.innerHTML = `${parseInt(res, 10).toString(2)}`;             
+            break;
+
+        default:
+            break;
+    }
 }
